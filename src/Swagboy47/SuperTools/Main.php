b473e6865389e517802779ba0902f19dbcfa356b
swagking47 <boyswag47@gmail.com>
2014-07-15 05:15:22 -0400

    Create Main.php

---
+++ src/Swagboy47/SuperTools/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Swagboy47\SuperTools;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\inventory;
+use pocketmine\inventory\PlayerInventory;
+use pocketmine\block\GoldOre;
+use pocketmine\block\IronOre;
+use pocketmine\entity\Entity;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use 
